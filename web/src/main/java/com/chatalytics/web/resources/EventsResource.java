ccf2651cac824cef3e1776a3725e7b48313ec325
Giannis Neokleous <giannis@knewton.com>
2016-04-13 10:26:02 -0400

    Don't close all the sessions in server sockets
    - The realtime compute and web resources were closing all sessions instead of the one that was initiating a close session. Fix that

--- web/src/main/java/com/chatalytics/web/resources/EventsResource.java
+++ web/src/main/java/com/chatalytics/web/resources/EventsResource.java
@@ -64,15 +64,22 @@ public class EventsResource {
         }
     }
 
+    /**
+     * Closes a session
+     *
+     * @param session
+     *            The session to close
+     * @param reason
+     *            The reason for closing
+     */
     @OnClose
-    public void close(CloseReason reason) {
-        LOG.info("Closing {} sessions", sessions.size());
-        for (Session session : sessions) {
-            try {
-                session.close();
-            } catch (IOException e) {
-                LOG.warn("Couldn't close {}", session.getId());
-            }
+    public void close(Session session, CloseReason reason) {
+        LOG.info("Closing session {}. Reason {}", session.getId(), reason);
+        try {
+            session.close();
+            sessions.remove(session);
+        } catch (IOException e) {
+            LOG.warn("Couldn't close {}", session.getId());
         }
     }
 
