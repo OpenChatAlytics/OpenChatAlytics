af4874d0caf424dc188156d6b0b228f769c442be
Giannis Neokleous <giannis@knewton.com>
2016-04-17 10:26:34 -0400

    ChatAlyticsBaseBolt that instantiates a config
    - All the bolts were pulling the ChatAlyticsConfig from the storm conf. Create a base bolt that
        inherits from BaseRichBolt and that instantiates a ChatAlyticsConfig

---
+++ compute/src/main/java/com/chatalytics/compute/storm/bolt/ChatAlyticsBaseBolt.java
@@ -0,0 +1,43 @@
+package com.chatalytics.compute.storm.bolt;
+
+import com.chatalytics.compute.config.ConfigurationConstants;
+import com.chatalytics.compute.util.YamlUtils;
+import com.chatalytics.core.config.ChatAlyticsConfig;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.base.BaseRichBolt;
+
+import java.util.Map;
+
+/**
+ * Inherit from this bolt if you want your prepare method to be called with a
+ * {@link ChatAlyticsConfig}
+ *
+ * @author giannis
+ */
+public abstract class ChatAlyticsBaseBolt extends BaseRichBolt {
+
+    private static final long serialVersionUID = -7961960405946887688L;
+
+    @Override
+    public void prepare(@SuppressWarnings("rawtypes") Map stormConf, TopologyContext context,
+            OutputCollector collector) {
+        String configStr = (String) stormConf.get(ConfigurationConstants.CHATALYTICS_CONFIG.txt);
+        ChatAlyticsConfig config = YamlUtils.readYamlFromString(configStr, ChatAlyticsConfig.class);
+        prepare(config, stormConf, context, collector);
+    }
+
+    /**
+     * Prepare method to implement that also passes a {@link ChatAlyticsConfig}
+     *
+     * @param config The {@link ChatAlyticsConfig}
+     * @param stormConf The storm config
+     * @param context Topology context
+     * @param collector The storm collector
+     */
+    public abstract void prepare(ChatAlyticsConfig config,
+            @SuppressWarnings("rawtypes") Map stormConf, TopologyContext context,
+            OutputCollector collector);
+
+}
