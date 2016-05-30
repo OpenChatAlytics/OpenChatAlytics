5c7bae93c484c9ddb7c25b7fe892de4787aca9d1
Giannis Neokleous <giannis@knewton.com>
2016-05-30 11:39:33 -0400

    Setting timezone in persistence.xml
    - Setting a timezone for the databse to be UTC
    
    - Adding more tests in the entities resource

--- web/src/test/java/com/chatalytics/web/resources/EntitiesResourceTest.java
+++ web/src/test/java/com/chatalytics/web/resources/EntitiesResourceTest.java
@@ -9,6 +9,7 @@ import com.chatalytics.core.similarity.SimilarityDimension;
 import com.chatalytics.web.utils.DateTimeUtils;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -19,8 +20,12 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests {@link EntitiesResource}.
@@ -34,10 +39,12 @@ public class EntitiesResourceTest {
     private EntitiesResource underTest;
     private DateTimeZone dtZone;
     private DateTime mentionTime;
+    private List<ChatEntity> entities;
+    private ChatAlyticsConfig config;
 
     @Before
     public void setUp() throws Exception {
-        ChatAlyticsConfig config = new ChatAlyticsConfig();
+        config = new ChatAlyticsConfig();
         config.persistenceUnitName = "chatalytics-web-test";
         config.timeZone = "America/New_York";
         dtZone = DateTimeZone.forID(config.timeZone);
@@ -46,7 +53,7 @@ public class EntitiesResourceTest {
         entityDao.startAsync().awaitRunning();
 
         mentionTime = DateTime.now().withZone(DateTimeZone.UTC);
-        List<ChatEntity> entities = Lists.newArrayListWithCapacity(10);
+        this.entities = Lists.newArrayListWithCapacity(10);
         entities.add(new ChatEntity("e1", 5, mentionTime.minusHours(1), "u1", "r1"));
         entities.add(new ChatEntity("e1", 4, mentionTime.minusHours(1), "u2", "r1"));
         entities.add(new ChatEntity("e1", 1, mentionTime.minusHours(2), "u3", "r2"));
@@ -98,11 +105,26 @@ public class EntitiesResourceTest {
         assertEquals(expected, response);
     }
 
+    @Test
+    public void testGetAllEntities() throws Exception {
+        DateTimeFormatter dtf = DateTimeUtils.PARAMETER_WITH_DAY_DTF.withZone(dtZone);
+        String startTimeStr = dtf.print(mentionTime.minusDays(1));
+        String endTimeStr = dtf.print(mentionTime.plusDays(1));
+        List<ChatEntity> result = underTest.getAllEntites(startTimeStr, endTimeStr, null, null);
+        assertEquals(entities.size(), result.size());
+
+        Set<ChatEntity> resultEntitySet = Sets.newHashSet(result);
+        for (ChatEntity expectedEntity : entities) {
+            assertTrue(resultEntitySet.contains(expectedEntity));
+        }
+
+    }
+
     /**
      * Tests the similarities endpoint
      */
     @Test
-    public void testGetSimilarities_RoomByEntity() {
+    public void testGetSimilarities_RoomByEntity() throws Exception {
         DateTimeFormatter dtf = DateTimeUtils.PARAMETER_WITH_DAY_DTF.withZone(dtZone);
         String startTimeStr = dtf.print(mentionTime.minusDays(1));
         String endTimeStr = dtf.print(mentionTime.plusDays(1));
@@ -114,8 +136,23 @@ public class EntitiesResourceTest {
         assertEquals(4, ldm.getMatrix().length);
     }
 
+    @Test(expected = UnsupportedOperationException.class)
+    public void testGetSimilarities_badCombination() throws Exception {
+        DateTimeFormatter dtf = DateTimeUtils.PARAMETER_WITH_DAY_DTF.withZone(dtZone);
+        String startTimeStr = dtf.print(mentionTime.minusDays(1));
+        String endTimeStr = dtf.print(mentionTime.plusDays(1));
+        underTest.getSimilarities(startTimeStr, endTimeStr,
+                                  SimilarityDimension.ROOM.getDimensionName(),
+                                  SimilarityDimension.ROOM.getDimensionName());
+    }
+
     @After
     public void tearDown() throws Exception {
+        EntityManager em = ChatAlyticsDAOFactory.getEntityManagerFactory(config)
+                                                .createEntityManager();
+        em.getTransaction().begin();
+        em.createNativeQuery("DELETE FROM " + ChatEntity.ENTITY_TABLE_NAME).executeUpdate();
+        em.getTransaction().commit();
         entityDao.stopAsync().awaitTerminated();
     }
 
