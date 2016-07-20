48c51c11626c3516fb056f459c17ff2aeb33b43d
Giannis Neokleous <giannis@knewton.com>
2016-07-20 19:10:25 -0400

    Don't leak connections in the ChatAlyticsDAO

--- compute/src/main/java/com/chatalytics/compute/db/dao/ChatAlyticsDAOImpl.java
+++ compute/src/main/java/com/chatalytics/compute/db/dao/ChatAlyticsDAOImpl.java
@@ -19,10 +19,10 @@ import javax.persistence.EntityManagerFactory;
  */
 public class ChatAlyticsDAOImpl extends AbstractIdleService implements IChatAlyticsDAO {
 
-    private final EntityManager entityManager;
+    private final EntityManagerFactory entityManagerFactory;
 
     public ChatAlyticsDAOImpl(EntityManagerFactory entityManagerFactory) {
-        this.entityManager = entityManagerFactory.createEntityManager();
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     /**
@@ -31,13 +31,20 @@ public class ChatAlyticsDAOImpl extends AbstractIdleService implements IChatAlyt
     @Override
     public DateTime getLastMessagePullTime() {
         String query = String.format("FROM %s", LastPullTime.class.getSimpleName());
-        List<LastPullTime> result = entityManager.createQuery(query, LastPullTime.class)
-                                                 .getResultList();
 
-        if (result == null || result.size() == 0) {
-            return new DateTime(0).withZone(DateTimeZone.UTC);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        try {
+            List<LastPullTime> result = entityManager.createQuery(query, LastPullTime.class)
+                                                     .getResultList();
+
+            if (result == null || result.size() == 0) {
+                return new DateTime(0).withZone(DateTimeZone.UTC);
+            }
+            return result.get(0).getTime();
+        } finally {
+            entityManager.close();
         }
-        return result.get(0).getTime();
     }
 
     /**
@@ -48,24 +55,26 @@ public class ChatAlyticsDAOImpl extends AbstractIdleService implements IChatAlyt
      */
     @Override
     public void setLastMessagePullTime(DateTime time) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
         LastPullTime lastPullTime = entityManager.find(LastPullTime.class, LastPullTime.ID);
-
-        entityManager.getTransaction().begin();
-        if (lastPullTime != null) {
-            lastPullTime.setTime(time);
-        } else {
-            lastPullTime = new LastPullTime(time);
-            entityManager.persist(lastPullTime);
+        try {
+            entityManager.getTransaction().begin();
+            if (lastPullTime != null) {
+                lastPullTime.setTime(time);
+            } else {
+                lastPullTime = new LastPullTime(time);
+                entityManager.persist(lastPullTime);
+            }
+            entityManager.getTransaction().commit();
+        } finally {
+            entityManager.close();
         }
-        entityManager.getTransaction().commit();
     }
 
     @Override
-    protected void shutDown() throws Exception {
-        entityManager.close();
-    }
+    protected void shutDown() throws Exception { }
 
     @Override
-    protected void startUp() throws Exception {
-    }
+    protected void startUp() throws Exception { }
 }
