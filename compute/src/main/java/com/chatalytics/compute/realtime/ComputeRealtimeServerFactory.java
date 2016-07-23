94c55df9541d4a4b9aa18ddd2ec7408f518221b5
Giannis Neokleous <giannis@knewton.com>
2016-07-23 10:10:07 -0400

    Adding status and health endpoint for compute
    - Compute now has a status endpoint with a single health path for now

--- compute/src/main/java/com/chatalytics/compute/realtime/ComputeRealtimeServerFactory.java
+++ compute/src/main/java/com/chatalytics/compute/realtime/ComputeRealtimeServerFactory.java
@@ -1,9 +1,13 @@
 package com.chatalytics.compute.realtime;
 
+import com.chatalytics.compute.web.resources.StatusResource;
 import com.chatalytics.core.config.ChatAlyticsConfig;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
 
 import javax.servlet.ServletException;
@@ -28,8 +32,11 @@ public class ComputeRealtimeServerFactory {
         Server server = new Server(config.computeConfig.rtComputePort);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        server.setHandler(context);
 
+        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitParameter(PackagesResourceConfig.PROPERTY_PACKAGES,
+                                       StatusResource.class.getPackage().toString());
+        server.setHandler(context);
         ServerContainer wscontainer;
         try {
             wscontainer = WebSocketServerContainerInitializer.configureContext(context);
